013e5f3cd8c5634b7d24369f9bf94d353e3d4174
RLant <rebeccalant@gmail.com>
2021-03-30 22:49:32 -0400

    Modulo understanding

--- scripts.js
+++ scripts.js
@@ -9,6 +9,8 @@ $(function(){
   ];
   
   let i = 0;
+
+  //The set interval function is selfinvoking. 
   setInterval(function(){
    
     i = (i + 1) % images.length;
@@ -24,4 +26,9 @@ $(function(){
     
 });
 
+// To understand the modulo, just know that when the number is greater on the right side, you have to ask how many times the number on the right can go into the one on the left. If it cannot go into the number, the remainder is the number on the left. 
+
+// (0 + 1) 1 % 3 = 1
+// (1 + 1) 2 % 3 = 2
+// (2 + 1) 3 % 3 = 0
 
