c325ec4b5f5f39c32642506bdc1b2b46f00ef937
RLant <rebeccalant@gmail.com>
2021-03-30 22:51:39 -0400

    notes on modulo

--- scripts.js
+++ scripts.js
@@ -28,6 +28,8 @@ $(function(){
 
 // To understand the modulo, just know that when the number is greater on the right side, you have to ask how many times the number on the right can go into the one on the left. If it cannot go into the number, the remainder is the number on the left. 
 
+// Modulos are not used very often, but when you see them they are usually used for little nifty tricks like this or situations where you need to find odd vs even #s. 
+
 // (0 + 1) 1 % 3 = 1
 // (1 + 1) 2 % 3 = 2
 // (2 + 1) 3 % 3 = 0
